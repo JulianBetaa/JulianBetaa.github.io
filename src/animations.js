fa2f05f1bfdf4eab952d0f27c0a4a8051f8b87d1
JulianBetaa <sievertjulian@gmail.com>
2024-05-20 18:32:48 +0200

    Room

--- src/animations.js
+++ src/animations.js
@@ -1,5 +1,13 @@
 export function animateTorus(delta, torus) {
-    torus.rotation.x += 0.2 * delta;
-    torus.rotation.y += 0.1 * delta;
-    torus.rotation.z += 0.2 * delta;
+    torus.rotation.x += 0.27 * delta;
+    torus.rotation.y += 0.13 * delta;
+    torus.rotation.z += 0.29 * delta;
+}
+export function animateCamera(delta, camera, move, cameraMoveSpeed) {
+    if (move.forward) camera.position.z -= cameraMoveSpeed * delta;
+    if (move.backward) camera.position.z += cameraMoveSpeed * delta;
+    if (move.left) camera.position.x -= cameraMoveSpeed * delta;
+    if (move.right) camera.position.x += cameraMoveSpeed * delta;
+    if (move.up) camera.position.y += cameraMoveSpeed * delta;
+    if (move.down) camera.position.y -= cameraMoveSpeed * delta;
 }
